5d734bb0f71d6866e4e19b0a23041adef2c952ed
BrianCompau <brcompau@gmail.com>
2024-10-25 19:55:14 -0400

    added player animations

--- 2024Spooky2DJamProject/Assets/Scripts/Player.cs
+++ 2024Spooky2DJamProject/Assets/Scripts/Player.cs
@@ -2,19 +2,50 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Vector2 screenBounds;
+    private Animator animator;
+    //dir: 0 = up, 1 = downl, 2 = left, 3 = right
+    private int dir;
+    private bool facingRight;
     public int moveSpeed = 4;
     public int hunger;
 
     void Start() {
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        animator = GetComponent<Animator>();
+        facingRight = true;
     }
 
     // Update is called once per frame
     void Update() {
         Vector2 playerPos = transform.position;
-        //keyboard input
-        Vector2 input = new(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        Vector2 displacement = 0.001f * moveSpeed * Vector3.Normalize(input);
+
+        var vertical = Input.GetAxisRaw("Vertical");
+        var horizontal = Input.GetAxisRaw("Horizontal");
+
+        Vector2 direction = new(0,0);
+        if (horizontal > 0) {
+            direction.x = 1;
+            if (!facingRight) {
+                gameObject.transform.localScale = new Vector3(1,1,1);
+                facingRight = true;
+            }
+        }
+        else if (horizontal < 0) {
+            direction.x = -1;
+            if (facingRight) {
+                gameObject.transform.localScale = new Vector3(-1,1,1);
+                facingRight = false;
+            }
+        }
+        else if (vertical > 0) {
+            direction.y = 1;
+        }
+        else if (vertical < 0) {
+            direction.y = -1;
+        }
+        setAnimation(direction);
+
+        Vector2 displacement = 0.001f * moveSpeed * direction;
         playerPos += displacement;
 
         //keep player within screen bounds
@@ -22,5 +53,12 @@ public class Player : MonoBehaviour
         playerPos.y = Mathf.Clamp(playerPos.y, -1 * screenBounds.y, screenBounds.y);
 
         transform.position = playerPos;
+    }
+
+    void setAnimation(Vector2 direction) {
+        animator.SetBool("idle", direction.x == direction.y);
+        animator.SetBool("walkUp", direction.y == 1);
+        animator.SetBool("walkDown", direction.y == -1);
+        animator.SetBool("walkSide", direction.x != 0);
     }
 }
