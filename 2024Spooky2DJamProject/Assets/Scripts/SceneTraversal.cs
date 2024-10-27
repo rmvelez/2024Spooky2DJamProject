665e7397f8986b190712e1f31d28fba8c0f7d022
BrianCompau <brcompau@gmail.com>
2024-10-27 14:23:07 -0400

    force resolution

--- 2024Spooky2DJamProject/Assets/Scripts/SceneTraversal.cs
+++ 2024Spooky2DJamProject/Assets/Scripts/SceneTraversal.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class SceneTraversal : MonoBehaviour
 {
+    void Start() {
+        Screen.SetResolution(1080, 1080, true);
+    }
+
     public void SlideEnd() {
         var player = GameObject.FindWithTag("Player").GetComponent<Player>();
         player.SlideEnd();
